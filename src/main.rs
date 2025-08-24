4dea1f236c213537d0cc7cbb01172a4131ef9c1c
jackkendall <jkendall3096@gmail.com>
2025-08-24 12:10:52 +0100

    Allow cancellation

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@
 
 use std::env;
 
-use crate::{config::ScheduleItem, schedule::Invocation, volume::VolumeSetter};
+use tokio::signal;
+use tracing::info;
+
+use crate::{
+    config::ScheduleItem,
+    schedule::{Invocation, Schedule, ScheduleError},
+    volume::VolumeSetter,
+};
 
 mod config;
 mod schedule;
@@ -11,6 +18,51 @@ mod volume;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt().with_target(false).init();
+
+    info!("starting up");
+
+    let mut schedule = get_schedule().await?;
+
+    info!("built schedule");
+
+    let mut next = schedule.get_next().ok_or("No schedule was established")?;
+
+    wait_for_next(&next).await;
+
+    let changer = volume::system_volume();
+
+    let worker = async {
+        loop {
+            println!("Invoking...!");
+
+            changer.process(next).await;
+
+            next = schedule
+                .get_next()
+                .expect("This method should always succeed if it has succeeded once before");
+
+            wait_for_next(&next).await;
+        }
+    };
+
+    tokio::select! {
+        _ = worker => {}
+        _ = signal::ctrl_c() => {
+            info!("shutdown signal received");
+        }
+    }
+
+    info!("clean exit");
+
+    Ok(())
+}
+
+async fn wait_for_next(invocation: &Invocation) {
+    tokio::time::sleep_until(invocation.get_start().into()).await;
+}
+
+async fn get_schedule() -> Result<Schedule, ScheduleError> {
     let args: Vec<String> = env::args().collect();
 
     let default_schedule = vec![
@@ -33,27 +85,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             schedule: default_schedule,
         });
 
-    let mut schedule = schedule::Schedule::from_schedule_items(config.schedule)?;
-
-    let mut next = schedule.get_next().ok_or("No schedule was established")?;
-
-    wait_for_next(&next).await;
-
-    let changer = volume::system_volume();
-
-    loop {
-        println!("Invoking...!");
-
-        changer.process(next).await;
-
-        next = schedule
-            .get_next()
-            .expect("This method should always succeed if it has succeeded once before");
-
-        wait_for_next(&next).await;
-    }
-}
-
-async fn wait_for_next(invocation: &Invocation) {
-    tokio::time::sleep_until(invocation.get_start().into()).await;
+    schedule::Schedule::from_schedule_items(config.schedule)
 }
